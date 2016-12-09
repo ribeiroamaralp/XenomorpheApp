ffda7d584059b35979807eace9e52730c4c29bf7
Adrien HEG <Adrien HEG>
2016-12-09 16:48:52 +0100

    commit dernier

--- www/js/listeJeux.js
+++ www/js/listeJeux.js
@@ -3,7 +3,7 @@ function getAllGames() {
     $.get(apiHost + '/wp/v2/xenogame?per_page=1000')
         .then(function (response) {
             $.each(response,function() {
-               $("#listViewJeux").append("<a class='list-group-item allow-badge widget uib_w_13' data-uib='twitter%20bootstrap/list_item' data-ver='1'><h4 class='list-group-item-heading'>"+$(this)[0].title.rendered+"</h4><p class='list-group-item-text'><img src='"+$(this)[0].featured_image_thumbnail_url+"' /></p></a>");
+               $("#listViewJeux").append("<a class='list-group-item allow-badge widget uib_w_13' data-uib='twitter%20bootstrap/list_item' data-ver='1'><p class='list-group-item-text'><img width='70px'  style='float : left' src='"+$(this)[0].featured_image_thumbnail_url+"' /><h4 class='list-group-item-heading'>"+$(this)[0].title.rendered+"</h4></p></a>");
             });
         })
 };
